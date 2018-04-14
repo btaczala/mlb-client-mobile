7d1536ea9595ff04ab29b86cd9b7e8196975edf2
Bartosz Taczała <bartosz.taczala@mobica.com>
2018-04-14 11:29:21 +0200

    small fixes

--- android_env.sh
+++ android_env.sh
@@ -10,28 +10,33 @@ export ANDROID_TARGET_ABI=armv7
 export ANDROID_NDK_ROOT=$android_root_dir/ndk/android-ndk-r13b
 
 function android_x86_qmake() {
-     PATH=/mnt/external/projects/android/qt5/5.10.1/android_x86/bin/ qmake $@
+     PATH=$android_root_dir/qt5/5.10.1/android_x86/bin/ qmake $@
 }
 
 function android_arm_qmake() {
-     PATH=/mnt/external/projects/android/qt5/5.10.1/android_armv7/bin/ qmake $@
+     PATH=$android_root_dir/qt5/5.10.1/android_armv7/bin/ qmake $@
 }
 
 function android_build_apk() {
     arch=$1
-    build_dir=$(mktemp -d)
+    build_dir=$2
     make install INSTALL_ROOT=$build_dir
     $android_root_dir/qt5/5.10.1/android_$arch/bin/androiddeployqt --input android-libmobile-client.so-deployment-settings.json --output $build_dir --deployment bundled --gradle --verbose
 }
 
 function mlb_build_x86() {
+    build_dir=$(mktemp -d)
     android_x86_qmake .. CONFIG+=debug
     make -j$(nproc)
-    android_build_apk x86 
+    android_build_apk x86 $build_dir
+
+    apk_file=$(find $build_dir -name *.apk)
+    echo "APK File under $apk_file"
 }
 
 function mlb_build_arm() {
+    build_dir=$(mktemp -d)
     android_arm_qmake .. CONFIG+=debug
     make -j$(nproc)
-    android_build_apk armv7
+    android_build_apk armv7 $build_dir
 }
